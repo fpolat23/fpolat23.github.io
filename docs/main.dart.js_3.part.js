83ec4d70234e428adbfa89c269f2d4bdcbb8af21
fpolat23 <fpolat23@gmail.com>
2026-05-12 20:39:07 +0300

    Add docs folder copy for GitHub Pages deployment

---
+++ docs/main.dart.js_3.part.js
@@ -0,0 +1,12 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={
+a(d){return new A.diX(d)},
+diX:function diX(d){this.a=d}}
+B=c[0]
+A=a.updateHolder(c[108],A)
+var z=a.updateTypes([])
+A.diX.prototype={
+$0(){return this.a},
+$S:23};(function inheritance(){var y=a.inherit
+y(A.diX,B.aVz)})()};
+(a=>{a["wH6DnJcSseD1rxKxYF4AK+Yeh/g="]=a.current})($__dart_deferred_initializers__);
